61ae4872134faec64d2385525936f4f571ccac6e
huangchucai <425205839@qq.com>
2018-10-24 20:43:50 +0800

    基本开始

---
+++ components/like/index.js
@@ -0,0 +1,31 @@
+// components/like/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    likeNumber: {
+      type: Number,
+      value: 0
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    imageSrc: 'images/like.png'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onlike(e) {
+      this.setData({
+        data:
+      })
+
+    }
+  }
+})
